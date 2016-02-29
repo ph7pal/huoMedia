a3ae6db3554b7021fb0440af2d1b93343e49c2fd
dafei-win10 <ph7pal@qq.com>
2016-02-29 21:29:41 +0800

    backup

--- themes/web/views/layouts/common.php
+++ themes/web/views/layouts/common.php
@@ -2,6 +2,14 @@
 <html lang="zh-CN">
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /> 
+        <meta name="viewport" content="width=device-width,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no">
+        <meta name="viewport" content="initial-scale=1.0,user-scalable=no,maximum-scale=1">
+        <meta content="yes" name="apple-mobile-web-app-capable" />
+        <meta content="black" name="apple-mobile-web-app-status-bar-style"  />
+        <meta name="apple-touch-fullscreen" content="yes">
+        <meta name="full-screen" content="yes">
+        <meta name="format-detection" content="telephone=no">    
+        <meta name="format-detection" content="address=no">
         <meta name="renderer" content="webkit"/>
         <meta name="keywords" content="<?php if (!empty($this->keywords)){echo $this->keywords;}else{ echo zmf::config('siteKeywords');}?>" />
         <meta name="description" content="<?php if (!empty($this->pageDescription)){echo $this->pageDescription;}else{ echo zmf::config('siteDesc');}?>" />
